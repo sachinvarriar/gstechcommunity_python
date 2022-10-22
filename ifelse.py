b24950dc8ede188ddf30eadbdf5a53be310a21d2
sachinvarriar <sachin.varriar@gmail.com>
2022-10-22 15:24:47 +0000

    live changes during session

--- ifelse.py
+++ ifelse.py
@@ -1,7 +1,9 @@
 name = input()
 password = input()
-if name == 'Mary':
+if name == 'Mary' or 'Joe':
     if password == 'swordfish':
+      print('Access Granted!')
+    elif password == 'goldfish':
       print('Access Granted!')
     else:
       print('Access Denied! Wrong password')
--- while.py
+++ while.py
@@ -1,4 +1,5 @@
 spam = 0
-while spam < 5:
+while spam <= 5:
     print('Hello, world.')
+    print(spam)
     spam = spam + 1
